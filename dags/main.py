88a750de7a8b0e95e9c89b3b4bf49ce5c9ebca44
Luke Lavanway <luke.lavanway@gmail.com>
2025-02-13 23:52:22 -0500

    Adjust retry delay

--- dags/main.py
+++ dags/main.py
@@ -55,7 +55,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=2)
+    'retry_delay': timedelta(seconds=10)
 }
 
 def get_gcs_client():
