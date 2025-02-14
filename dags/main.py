557325c9bc5077da4543592ba21fbbc4d859f0a8
Luke Lavanway <luke.lavanway@gmail.com>
2025-02-13 19:17:47 -0500

    Filenames

--- dags/main.py
+++ dags/main.py
@@ -107,7 +107,12 @@ def download_and_extract_to_gcs(url: str, bucket_name: str, blob_path: str) -> N
             
         except BadZipFile:
             # Handle as single file
-            filename = os.path.basename(url)
+            if "Content-Disposition" in response.headers:
+                content_disposition = response.headers["Content-Disposition"]
+                    if "filename=" in content_disposition:
+                        filename = content_disposition.split("filename=")[1].strip('"')
+            else:
+                filename = 'file_{}'.format(datetime.now())
             blob = bucket.blob(f"{blob_path}/{filename}")
             blob.upload_from_string(response.content) 
             logger.info(f"Successfully uploaded 1 file to {blob_path}")
